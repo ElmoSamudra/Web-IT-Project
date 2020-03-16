bfb81d26eed26117985f97bf3ece20a095905c47
kwaluyo <61862757+kwaluyo@users.noreply.github.com>
2020-03-16 16:32:29 +1100

    Delete alertKartika.js

--- alertKartika.js
+++
@@ -1,6 +0,0 @@
-<script>
-    function submitButton() {
-    alert(document.getElementById("formname").elements['firstname'].value + " " +
-    document.getElementById("formname").elements['lastname'].value)
-    }
-</script>
