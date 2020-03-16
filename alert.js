893d94992df41bde3306ede2bd16ac3817512888
Elmo <Shawnsamudra09@users.noreply.github.com>
2020-03-16 15:55:18 +1100

    test

--- alert.js
+++ alert.js
@@ -1,12 +1,4 @@
-function getFormValue() {
-
-    var x = document.getElementById("formname");
-    var text = "";
-    var i;
-    for (i = 0; i < x.length-1 ;i++) {
-        text += x.elements[i].value + " ";
-    }
-    alert(text)
-
-
+function getFormValue(){
+    alert(document.getElementById("formname").elements["firstname"].value
+    + " " + document.getElementById("formname").elements["lastname"].value);
 }
---
+++ ws3js.js
@@ -0,0 +1,31 @@
+let word = window.word; //word to be guessed
+let gameState = {	
+  correctGuesses : [],   // may be use to store info relating to correct guesses, e.g. letters or indexes 
+  incorrectGuesses : [], // store incorrect letters
+  remainingGuesses : 10  // keep track of number of attempts
+};
+
+function setWord () {
+	
+}
+
+
+function validate (guess) {
+  
+}
+
+function check (guess) {
+
+}
+
+function updateWordTiles (index) {
+  
+}
+
+function checkEndOfGame() {
+  
+}
+
+function init () {
+  
+}
