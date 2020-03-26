e09940b1368fa6e354c139920eb21711d4e2db7c
Elmo <Shawnsamudra09@users.noreply.github.com>
2020-03-26 17:55:34 +1100

    start new

--- alert.js
+++
@@ -1,6 +0,0 @@
-<script>
-    function submitButton() {
-    alert(document.getElementById("formname").elements['firstname'].value + " test " +
-    document.getElementById("formname").elements['lastname'].value)
-    }  
-</script>
