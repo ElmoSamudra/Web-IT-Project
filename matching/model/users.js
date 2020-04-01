0daf524cd346810efd98dc3b5269b0290add4d47
nyoewono <nathanael9721@gmail.com>
2020-04-01 20:20:46 +1100

    first implementation matching

---
+++ matching/model/users.js
@@ -0,0 +1,64 @@
+var users = [
+    {
+        "id":"10001",
+        "username":"manthony",
+        "first_name":"Mike",
+        "last_name":"Anthony",
+        "gender":"Male",
+        "nationality":"Indonesian",
+        "hobby":["Basketball", "Cycling", "Jogging"],
+        "language":["English, Indonesia"],
+        "prefer_stay":"CBD",
+        "roommee":"Michael Leo"
+    },
+    {
+        "id":"10002",
+        "username":"mleo",
+        "first_name":"Michael",
+        "last_name":"Leo",
+        "gender":"Male",
+        "nationality":"Australian",
+        "hobby":["Bowling", "Photography"],
+        "language":["English"],
+        "prefer_stay":"Clayton",
+        "roommee":"Mike Anthony"
+    },
+    {
+        "id":"10003",
+        "username":"krafael",
+        "first_name":"Kenneth",
+        "last_name":"Rafael",
+        "gender":"Male",
+        "nationality":"Indonesian",
+        "hobby":["Gym", "Swimming"],
+        "language":["English, Indonesia"],
+        "prefer_stay":"CBD",
+        "roommee":"none"
+    },
+    {
+        "id":"10004",
+        "username":"jlim",
+        "first_name":"Jessica",
+        "last_name":"Lim",
+        "gender":"Female",
+        "nationality":"Chinese",
+        "hobby":["Reading Books", "Cooking"],
+        "language":["Mandarin"],
+        "prefer_stay":"CBD",
+        "roommee":"none"
+    },
+    {
+        "id":"10005",
+        "username":"ssamudra",
+        "first_name":"Shawn",
+        "last_name":"Samudra",
+        "gender":"Male",
+        "nationality":"Indonesian",
+        "hobby":["Sleep", "Dance"],
+        "language":["English, Indonesia"],
+        "prefer_stay":"Carlton",
+        "roommee":"none"
+    }  
+]
+
+module.exports = users;
