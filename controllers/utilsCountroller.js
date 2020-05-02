2934a62dbf8d8332a54f450ee9cca1cabbec3149
kwaluyo <61862757+kwaluyo@users.noreply.github.com>
2020-05-02 21:31:23 +1000

    fixed utils button

--- controllers/utilsCountroller.js
+++ controllers/utilsCountroller.js
@@ -1,6 +1,6 @@
 const utils = require("../models/utils.js");
 var Lease = require("../models/leaseDB.js")
-
+var users = require("../models/userDB.js");
 
 const getAllUtils2 =(req,res) =>{
   res.render("utils.ejs", {utils:utils});
@@ -12,10 +12,15 @@ const getAllUtils2 =(req,res) =>{
 const chooseUtils = async function(req, res) {
   try {
     res.send("You have chosen this company");
+    const currentUserId = req.account._id;
+    const currentUserData = await users.findOne({'accountId':currentUserId});
+    const leaseId = currentUserData.leaseID;
+
+    await Lease.updateOne({ '_id': leaseId }, 
+                            {$push:{'utils': req.params.utilsId}}
+                            );
+
     
-    await Lease.updateOne({ accountId: req.account._id }, 
-                              {$push:{'utils': req.params.utilsId}}
-                              );
    
   }
   catch (e) {
